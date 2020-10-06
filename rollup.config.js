9bae435667e4ad31956fd0fd6ca48a4e3a0b7dd5
Ivan Plesskih <terma95@gmail.com>
2020-10-06 20:11:54 +0500

    Init version

---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+const commonJs = require('@rollup/plugin-commonjs')
+const {nodeResolve} = require('@rollup/plugin-node-resolve')
+const nodePolyfill = require('rollup-plugin-node-polyfills')
+
+const plugins = [
+    new commonJs(),
+    new nodeResolve({browser: true}),
+    new nodePolyfill(),
+]
+
+export default [{
+    input: 'src/postcss.js',
+    output: {
+        file: 'postcss.js',
+        format: 'umd',
+        name: 'postcss'
+    },
+    plugins
+}, {
+    input: 'src/plugins/nested.js',
+    output: {
+        file: 'plugins/nested.js',
+        format: 'umd',
+        name: 'postcssNested'
+    },
+    plugins
+}]
